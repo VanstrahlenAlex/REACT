01cee68077bbd9ff859267f39cd6a2c6a7c4fefc
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2022-06-06 11:40:30 -0500

    estudiando 6-06-2022

---
+++ linkedin-clone-yt/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+
+function App() {
+  return ( 
+  <div className = "App" >
+
+    {/* header */}
+    <Header />
+    {/* App body */}
+      {/* sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+  </div>
+    );
+}
+
+export default App;
---
+++ linkedin-clone-yt/src/Header.jsx
@@ -0,0 +1,41 @@
+// rfce
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Header() {
+  return (
+  <div className='header'>
+    <div className="header__left">
+      <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="" />
+      <LinkedInIcon />
+      <div className="header__search">
+        <SearchIcon />
+        <input type="text" />
+      </div>
+    </div>
+    <div className="header__right">
+    <HeaderOption Icon={HomeIcon} title='Home' />
+    <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+    <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+    <HeaderOption Icon={ChatIcon} title='Messaging'/>
+    <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+
+
+
+    <HeaderOption />
+
+    </div>
+    </div>
+  )
+
+}
+
+export default Header;
